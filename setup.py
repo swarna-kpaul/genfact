70b6903c13235e8a59d03aada8096c8bc70c3928
swarna kamal paul <swarna.kpaul@gmail.com>
2021-05-19 12:55:04 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: Apache-2.0 License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
